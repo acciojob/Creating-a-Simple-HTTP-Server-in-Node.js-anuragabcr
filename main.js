57b3695ec164ebe38f4da945229b30f8400afefa
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-07-09 07:41:33 +0530

    completed

--- main.js
+++ main.js
@@ -3,10 +3,13 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   // TODO: Send the ""Hello, world!"" response
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, world!');
 });
 
 server.listen(port, () => {
   // TODO: Log a message to the console when the server starts listening on the port
+  console.log(`Server listening on port ${port}`);
 });
 
 // DO NOT EDIT BELOW THIS LINE
